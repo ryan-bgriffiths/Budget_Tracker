9b7921a71289154ecfd2dc1785f0578597e657a9
wadegoh <whgoh8@gmail.com>
2026-04-22 16:44:15 -0700

    MonthlyBreakdown skeleton class, method stubs only, simple constructor

---
+++ src/MonthlyBreakdown.java
@@ -0,0 +1,25 @@
+//TODO: import all dependencies
+
+public class MonthlyBreakdown{
+	public String name;
+	public int year;
+	public list<Expense> monthlyExpenses;
+	
+	public MonthlyBreakdown(String name, int year, list<Expense> monthlyExpenses) {
+		this.name = name;
+		this.year = year;
+		this.monthlyExpenses = monthlyExpenses;
+	}
+	
+	public void displayProgressToGoal(Goal currentGoal) {
+		//TODO: method stub
+	}
+	
+	public void displayProgressOfDebt(Debt currentDebt) {
+		//TODO: method stub
+	}
+	
+	public void displayUpdates(list<Debt> activeDebt, list<Debt> inactiveDebt, list<Goal> activeGoals, list<Goal> completedGoals) {
+		//TODO: method stub
+	}
+}
