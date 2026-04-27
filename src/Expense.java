9bde0f573ea8f78fd859b3dffb42f221cfb9f32a
Quang <Quang@DESKTOP-1Q9DA1G>
2026-04-26 17:16:05 -0700

    Refactor Node in MonthlyBudget to store an Expense object and created Expense class

---
+++ src/Expense.java
@@ -0,0 +1,12 @@
+
+public class Expense {
+	double amount;
+	String transDescription;
+	String transDate;
+	
+	public Expense(double amount, String transDescription, String transDate) {
+		this.amount = amount;
+		this.transDescription = transDescription;
+		this.transDate = transDate;
+	}
+}
--- src/MonthlyBudget.java
+++ src/MonthlyBudget.java
@@ -1,50 +1,52 @@
 
 public class MonthlyBudget {
-	class Node{
-		double amount; // This will store the money value of the transaction.
-		String transDescription; // A short explanation by user
-		String transDate; // Date of the transaction
+	public static class Node{
+		Expense expense;
 		Node next; // Points to either next node or previous in the list
 		Node prev;
 		int month; // Stores the month
 		
-		Node(double amount, String transDescription, String transDate, int month){
-			this.amount = amount;
-			this.transDescription = transDescription;
-			this.transDate = transDate;
+		Node(Expense expense, int month){
+			
 			this.prev = null;
 			this.next = null;
 			this.month = month;
+			this.expense = expense;
 		}
 	}
 	Node[] head = new Node[12];
 	Node[] tail = new Node[12];
 	
 	public void AddTransaction(double amount, String transDescription, String transDate, int month) { //A method for adding a new transaction to the list
-		Node NewNode = new Node(amount, transDescription, transDate, month); // Creating a new node with the given data
-		int index = month - 1; //I think an array would be an effective way for a fixed size for only 12 months
-		if (head[index] == null) {
-			head[index] = NewNode;
-			tail[index] = NewNode;
-		} else {
-			tail[index].next = NewNode; //Current last node points to the new node
-			NewNode.prev = tail[index]; //New node points to old tail
-			tail[index] = NewNode;
-		}
-	}
-	public void DisplayTransaction(double amount, String transDescription, String transDate, int month) { //A method to print all transaction throughout the node from start to end
-		Node current = head[month - 1];
-		while (current != null) { //This will keep looping as long as current is pointing at a valid node
-			System.out.println("What is the amount? : " + current.amount);
-			System.out.println("Description? : " + current.transDescription);
-			System.out.println("Date? : " + current.transDate);
-			System.out.println("Month? : " + current.month);
-			System.out.println();
-			current = current.next; //Move to the next node
-		}
 		if (month < 1 || month > 12) {
 			System.out.println("Invalid Month");
 			return;
+		}
+		Expense expense = new Expense(amount, transDescription, transDate);// Creating a new node with the given data
+		Node newNode = new Node(expense, month);
+		int index = month - 1; //I think an array would be an effective way for a fixed size for only 12 months
+		if (head[index] == null) {
+			head[index] = newNode;
+			tail[index] = newNode;
+		} else {
+			tail[index].next = newNode; //Current last node points to the new node
+			newNode.prev = tail[index]; //New node points to old tail
+			tail[index] = newNode;
+		}
+	}
+	public void DisplayTransaction(int month) { //A method to print all transaction throughout the node from start to end
+		if (month < 1 || month > 12) { //I believe if month is = 0 or 13, this will crash (Month validation)
+			System.out.println("Invalid Month");
+			return;
+		}
+		Node current = head[month - 1];
+		while (current != null) { //This will keep looping as long as current is pointing at a valid node
+			System.out.println("What is the amount? : " + current.expense.amount);
+			System.out.println("Description? : " + current.expense.transDescription);
+			System.out.println("Date? : " + current.expense.transDate);
+			System.out.println("Month? : " + current.month);
+			System.out.println();
+			current = current.next; //Move to the next node
 		}
 	}
 }
