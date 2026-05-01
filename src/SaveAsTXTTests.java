ea734e69d08c995a1f13a3154ae2b22c6fd158f5
Amy Rocha <amy.rocha36@gmail.com>
2026-05-01 14:03:32 -0700

    personal issue resolution attempt

--- src/SaveAsTXTTests.java
+++ src/SaveAsTXTTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class saveAsTXTTest {
+class SaveAsTXTTests {
 
 	class dataStorageTest {
 		
